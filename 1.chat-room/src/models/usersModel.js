b5c0bf354210931ffa740a7535ac4b1a25b13829
Daniel <daniel.macias.gomez@gmail.com>
2021-05-25 20:44:07 +0200

    changes

--- 1.chat-room/src/models/usersModel.js
+++ 1.chat-room/src/models/usersModel.js
@@ -2,15 +2,15 @@ require("../mongodb/connection");
 const mongoose = require("mongoose");
 const muv = require("mongoose-unique-validator");
 
-const userSchema = new mongoose.Schema({
+const googleUserSchema = new mongoose.Schema({
     username: { type: String, index: true, unique: true },
     password: { type: String },
     creationDate: { type: Date, default: Date.now },
     lastLogin: { type: Date, default: Date.now }
 });
 
-userSchema.plugin(muv);
+googleUserSchema.plugin(muv);
 
-const User = mongoose.model("Users", userSchema);
+const User = mongoose.model("Users", googleUserSchema);
 
 module.exports = User;
